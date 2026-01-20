f2f1d07637ce157c936d8cbe5380f2cb9833cf81
Andrés Mendoza <ramirezmendoza482@gmail.com>
2026-01-20 16:35:02 -0600

    project modulation

--- califications.py
+++ califications.py
@@ -1,58 +1,73 @@
 import pandas as pd
 
-dataset = pd.read_csv("calificaciones.csv")
+def separate():#Separador
+    print("#" * 20)
+    print("")
 
-def row():
-    print(f"El número de registros en el dataset es: {len(dataset)}")
+def clean_data(df):#Limpieza de datos
+    df = df.copy()
+    df["calificacion"] = df.groupby(["matricula", "nombre"])["calificacion"].transform(lambda x: x.fillna(x.mean()))
+    separate()
+    return df
 
-def blank():
-    if dataset["calificacion"].isna().sum() != 0:
-        print(f"La cantidad de calificaciones faltantes es: {dataset["calificacion"].isna().sum()}")
+def row(df):#¿Cuántos registros tiene el dataset?
+    print(f"El número de registros en el dataset es: {len(df)}")
+    separate()
+
+def blank(df):#¿Hay calificaciones faltantes? ¿En dónde?
+    if df["calificacion"].isna().sum() != 0:
+        empty_sum = df["calificacion"].isna().sum()
+        empty_fields = df[df.isna().any(axis=1)]
+        print(f"La cantidad de calificaciones faltantes es: {empty_sum}")
         print("Los registros faltantes son los siguientes:")
-        print(dataset[dataset.isna().any(axis=1)])
+        print(empty_fields)
+        separate()
+    else:
+        print("No hay calificaciones faltantes")
+        separate()
 
-def individual_mean():
-    print(dataset.groupby(["matricula", "nombre"])["calificacion"].mean().reset_index().rename(
-        columns={"calificacion": "promedio"}))
+def individual_mean(df):#¿Cuál es el promedio por alumno?
+    ind_mean = df.groupby(["matricula", "nombre"])["calificacion"].mean().reset_index().rename(
+        columns={"calificacion": "promedio"})
+    print(ind_mean)
+    separate()
 
-def assignament_mean():
-    print(dataset.groupby(["materia"])["calificacion"].mean().reset_index().rename(columns={"calificacion": "promedio materia"}))
+def assignment_mean(df):#¿Cuál es el promedio por materia?
+    assign_mean = df.groupby(["materia"])["calificacion"].mean().reset_index().rename(columns={"calificacion": "promedio_materia"})
+    print(assign_mean)
+    separate()
 
-def failed_student():
-    new_dataset = dataset.groupby(["matricula", "nombre"])["calificacion"].mean().reset_index().rename(columns={"calificacion" : "promedio"})
-    print(new_dataset[new_dataset["promedio"] < 70])
+def failed_student(df):#¿Qué alumnos están reprobados (promedio < 70)?
+    new_dataset = df.groupby(["matricula", "nombre"])["calificacion"].mean().reset_index().rename(columns={"calificacion" : "promedio"})
+    fail_students = new_dataset[new_dataset["promedio"] < 70]
+    print(fail_students)
+    separate()
 
-def group_mean():
-    new_dataset = dataset.groupby("grupo")["calificacion"].mean().reset_index()
+def group_mean(df):#¿Cuál grupo tiene mejor promedio general?
+    new_dataset = df.groupby("grupo")["calificacion"].mean().reset_index()
+    better_mean = new_dataset.iloc[new_dataset["calificacion"].idxmax()]
     print("El grupo con mejor promedio general es: ")
-    print(new_dataset.iloc[new_dataset["calificacion"].idxmax()])
+    print(better_mean)
+    separate()
 
-def sum_group():
-    print(dataset.groupby("grupo")["matricula"].nunique().reset_index())
+def sum_group(df):#¿Cuántos alumnos hay por grupo?
+    students_group = df.groupby("grupo")["matricula"].nunique().reset_index(name="total_alumnos")
+    print(students_group)
+    separate()
 
-sum_group()
+def main():
+    dataset = pd.read_csv("calificaciones.csv")
 
-"""
-Preguntas que debes responder:
-¿Cuántos registros tiene el dataset? 
-R= 15
+    blank(dataset)
 
-¿Hay calificaciones faltantes? ¿En dónde?
-R=Sí, una. En el registro 10.
+    dataset_clean = clean_data(dataset)
 
-¿Cuál es el promedio por alumno?
-R=
+    row(dataset_clean)
+    individual_mean(dataset_clean)
+    assignment_mean(dataset_clean)
+    failed_student(dataset_clean)
+    group_mean(dataset_clean)
+    sum_group(dataset_clean)
 
-¿Cuál es el promedio por materia?
-R=
+main()
 
-¿Qué alumnos están reprobados (promedio < 70)?
-R=
-
-¿Cuál grupo tiene mejor promedio general?
-R=
-
-¿Cuántos alumnos hay por grupo?
-R=
-
-"""
